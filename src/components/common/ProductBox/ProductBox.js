f0cb636872ffc3a821989703fa8da8b49a334623
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-12-10 02:26:48 +0100

    Add state and styling of buttons: favourite and compare

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,7 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars, noPromoPrice }) => (
+const ProductBox = ({ name, price, promo, stars, noPromoPrice, heart, arrows }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -39,10 +39,10 @@ const ProductBox = ({ name, price, promo, stars, noPromoPrice }) => (
     <div className={styles.line}></div>
     <div className={styles.actions}>
       <div className={styles.outlines}>
-        <Button variant='outline'>
+        <Button variant='outline' className={heart ? styles.heart : ''}>
           <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
         </Button>
-        <Button variant='outline'>
+        <Button variant='outline' className={arrows ? styles.heart : ''}>
           <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
         </Button>
       </div>
@@ -65,6 +65,8 @@ ProductBox.propTypes = {
   promo: PropTypes.string,
   stars: PropTypes.number,
   noPromoPrice: PropTypes.string,
+  heart: PropTypes.bool,
+  arrows: PropTypes.bool,
 };
 
 export default ProductBox;
